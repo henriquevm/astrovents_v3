543ab37669c7511724c170634a57874854a2aa9a
Henrique <henriqueveigamoreira@gmail.com>
2023-06-10 17:19:29 -0300

    add StatusBar

--- astrovents_v3/App.tsx
+++ astrovents_v3/App.tsx
@@ -1,5 +1,5 @@
-import { ActivityIndicator} from 'react-native';
 import React from 'react'
+
 import { ThemeProvider } from 'styled-components/native'
 
 import Groups from './src/screens/Groups';
@@ -7,18 +7,23 @@ import theme from './src/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Comfortaa_700Bold, Comfortaa_400Regular } from '@expo-google-fonts/comfortaa';
 import { Loading } from './src/components/Loading';
+import { StatusBar } from 'react-native';
 
 
 export default function App() {
-  
-  const [loadsFonts] = useFonts({Roboto_400Regular, Roboto_700Bold, Comfortaa_700Bold, Comfortaa_400Regular});
+
+  const [loadsFonts] = useFonts({ Roboto_400Regular, Roboto_700Bold, Comfortaa_700Bold, Comfortaa_400Regular });
 
   return (
 
     <ThemeProvider theme={theme}>
-      
-      { !loadsFonts ? <Groups /> : <Loading/>}
-      
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {!loadsFonts ? <Groups /> : <Loading />}
+
     </ThemeProvider>
 
   );
